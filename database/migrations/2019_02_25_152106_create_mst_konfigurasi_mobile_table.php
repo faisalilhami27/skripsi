df37e857b3a8d13bfbecfc2e7dac3783f8e28f76
Muhamad Faisal Ilhami Akbar <faisal.153040044@mail.unpas.ac.id>
2019-02-25 15:21:38 +0700

    penambahan migration konfigurasi mobile

---
+++ database/migrations/2019_02_25_152106_create_mst_konfigurasi_mobile_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMstKonfigurasiMobileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mst_konfigurasi_mobile', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 50);
+            $table->text('images');
+            $table->text('deskripsi');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mst_konfigurasi_mobile');
+    }
+}
