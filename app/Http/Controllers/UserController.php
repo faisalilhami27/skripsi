48e9efea3ff2f4ea8b4f7d99c9e26cb5b464c498
Muhamad Faisal Ilhami Akbar <faisal.153040044@mail.unpas.ac.id>
2019-02-25 13:32:05 +0700

    fix bug user controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserRequest;
+use App\Models\KonfigurasiModel;
+use App\Models\UserLevelModel;
+use App\Models\UserModel;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Yajra\DataTables\DataTables;
+
+class UserController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('checkRole');
+    }
+
+    public function index()
+    {
+        $level = UserLevelModel::all();
+        $title = "Halaman Pengguna";
+        $deskripsi = "Halaman pengguna digunakan untuk mengelola data user";
+        return view('user.userView', compact('title', 'deskripsi', 'level'));
+    }
+
+    public function datatable()
+    {
+        $data = UserModel::with('userLevel')->get();
+        return DataTables::of($data)->addIndexColumn()->make(true);
+    }
+
+    public function store(UserRequest $request)
+    {
+        $nama = $request->nama;
+        $email = $request->email;
+        $password = Hash::make($request->password);
+        $level = $request->level;
+        $status = $request->status;
+        $images = $request->file('images');
+//        $fileName = $images->getClientOriginalName();
+//        $path = 'storage/img';
+//        $images->move($path, $fileName);
+
+        $insert = UserModel::create([
+            'nama' => $nama,
+            'email' => $email,
+            'password' => $password,
+            'id_user_level' => $level,
+            'status' => $status,
+            'images' => $images->store(
+                'img', 'public'
+            )
+        ]);
+
+        if ($insert) {
+            return response()->json(['status' => 200, 'msg' => 'Data berhasil ditambahkan']);
+        } else {
+            return response()->json(['status' => 449, 'msg' => 'Data gagal ditambahkan']);
+        }
+    }
+
+    public function edit(Request $request)
+    {
+        $id = $request->id;
+        $data = UserModel::findOrFail($id);
+
+        if ($data) {
+            return response()->json(['status' => 200, 'user' => $data]);
+        } else {
+            return response()->json(['status' => 449, 'msg' => "Data tidak ditemukan"]);
+        }
+    }
+
+    public function update(Request $request)
+    {
+        $request->validate([
+           'level' => 'required',
+           'status' => 'required'
+        ]);
+
+        $data = $request->all();
+        $id = $request['id'];
+
+        $update = UserModel::findOrFail($id)->update($data);
+
+        if ($update) {
+            return response()->json(['status' => 200, 'msg' => 'Data berhasil diubah']);
+        } else {
+            return response()->json(['status' => 449, 'msg' => 'Data gagal diubah']);
+        }
+    }
+
+    public function destroy(Request $request)
+    {
+        $id = $request->id;
+
+        $delete = UserModel::findOrFail($id)->delete();
+
+        if ($delete) {
+            return response()->json(['status' => 200, 'msg' => 'Data berhasil dihapus']);
+        } else {
+            return response()->json(['status' => 449, 'msg' => 'Data gagal dihapus']);
+        }
+    }
+
+    public function resetPassword(Request $request)
+    {
+        $id = $request->id;
+        $konfigurasi = KonfigurasiModel::first();
+        $password = Hash::make($konfigurasi->reset_password);
+
+        $data = [
+          'password' => $password
+        ];
+
+        $reset = UserModel::findOrFail($id)->update($data);
+
+        if ($reset) {
+            return response()->json(['status' => 200, 'msg' => 'Data berhasil direset']);
+        } else {
+            return response()->json(['status' => 449, 'msg' => 'Data gagal direset']);
+        }
+    }
+
+    public function cekEmail(Request $request)
+    {
+        $email = $request->email;
+        $cekEmail = UserModel::where('email', $email)->get();
+        $getEmail = $cekEmail->count();
+
+        if ($getEmail == 1) {
+            return response()->json(['status' => 449, 'msg' => 'email has been used']);
+        } else {
+            return response()->json(['status' => 200, 'msg' => 'email available']);
+        }
+    }
+}
