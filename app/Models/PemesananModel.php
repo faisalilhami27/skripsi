e345fefa1f556ae419215f109beccd61cfcf3968
Muhamad Faisal Ilhami Akbar <faisal.153040044@mail.unpas.ac.id>
2019-03-03 03:45:17 +0700

    perbaikan header view

--- app/Models/PemesananModel.php
+++ app/Models/PemesananModel.php
@@ -30,6 +30,6 @@ class PemesananModel extends Model
 
     public function customer()
     {
-        return $this->belongsTo(CustomerModel::class, 'id_customer');
+        return $this->hasOne(CustomerModel::class, 'id', 'id_customer');
     }
 }
