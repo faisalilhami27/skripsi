3277763c2eae9b3cae873539db326b67e9df96cd
Muhamad Faisal Ilhami Akbar <faisal.153040044@mail.unpas.ac.id>
2019-03-26 12:07:48 +0700

    penambahan pusher untuk realtime aplikasi

--- app/Http/Controllers/Api/PembayaranController.php
+++ app/Http/Controllers/Api/PembayaranController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api;
 use App\Models\KonfirmasiPembayaranModel;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 use Pusher\Pusher;
 
 class PembayaranController extends Controller
@@ -26,25 +27,32 @@ class PembayaranController extends Controller
                 'bukti_pembayaran' => url('storage') . '/' . $file->store('pembayaran/'. $kode .'', 'public')
             ];
 
-            $update = KonfirmasiPembayaranModel::where('kode_pemesanan', $kode)->update($data);
+            DB::beginTransaction();
+            try {
+                $update = KonfirmasiPembayaranModel::where('kode_pemesanan', $kode)->update($data);
 
-            if ($update) {
-                $options = array(
-                    'cluster' => 'ap1',
-                    'useTLS' => true
-                );
-                $pusher = new Pusher(
-                    'ca529096e60dc5ab5a37',
-                    '06eb93af4bceb9c2da38',
-                    '717606',
-                    $options
-                );
+                if ($update) {
+                    $options = array(
+                        'cluster' => 'ap1',
+                        'useTLS' => true
+                    );
+                    $pusher = new Pusher(
+                        'ca529096e60dc5ab5a37',
+                        '06eb93af4bceb9c2da38',
+                        '717606',
+                        $options
+                    );
 
-                $data1['message'] = 'hello world';
-                $pusher->trigger('my-channel1', 'my-event1', $data1);
-                return response()->json(["status" => 200, "user" => $data, "msg" => "Bukti pembayaran berhasil diupload"]);
-            } else {
-                return response()->json(["status" => 500, "msg" => "Data gagal diupload"]);
+                    $data1['message'] = 'hello world';
+                    $pusher->trigger('my-channel1', 'my-event1', $data1);
+                    DB::commit();
+                    return response()->json(["status" => 200, "user" => $data, "msg" => "Bukti pembayaran berhasil diupload"]);
+                } else {
+                    return response()->json(["status" => 500, "msg" => "Data gagal diupload"]);
+                }
+            } catch (\Exception $e) {
+                DB::rollBack();
+                return response()->json(['status' => 500, 'msg' => 'Server sedang sibuk silahkan ulangi beberapa saat lagi']);
             }
         }
     }
