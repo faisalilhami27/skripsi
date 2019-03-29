3367db50c2930fc8db8185fc7ce4fcd9d7291e2c
Muhamad Faisal Ilhami Akbar <faisal.153040044@mail.unpas.ac.id>
2019-03-30 06:57:21 +0700

    perbaikan auth api

--- app/Http/Controllers/Api/AuthenticationController.php
+++ app/Http/Controllers/Api/AuthenticationController.php
@@ -29,10 +29,14 @@ class AuthenticationController extends Controller
         switch ($this->request->loginAs) {
             case 'api_customer':
                 $loginAs = CustomerModel::where('username', $this->request->username)->first();
-                if ($this->request->player_id != $loginAs->player_id) {
-                    CustomerModel::where('username', $this->request->username)->update([
-                        'player_id' => $this->request->player_id
-                    ]);
+                if (!is_null($this->request->player_id) || $this->request->player_id != '') {
+                    if ($this->request->player_id != $loginAs->player_id) {
+                        CustomerModel::where('username', $this->request->username)->update([
+                            'player_id' => $this->request->player_id
+                        ]);
+                    }
+                } else {
+                    return "";
                 }
                 $user = $loginAs;
                 break;
